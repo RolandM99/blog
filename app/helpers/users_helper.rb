4e144848ffaa00fb29a645be430762a075c3010e
rolandm99 <manfulmweze99@gmail.com>
2022-03-30 13:44:13 +0100

    Add image link in helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def user_img(user)
+    user.photo.present? ? user.photo : 'https://p.kindpng.com/picc/s/544-5442212_download-free-icon-vectors-male-visio-user-icon.png'
+  end
 end
