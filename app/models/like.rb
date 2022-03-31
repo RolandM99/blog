2f03c74beac1e637225fe32c90efb1f715911566
rolandm99 <manfulmweze99@gmail.com>
2022-03-31 08:17:41 +0100

    Update models for comment and like

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User'
 
-  def update_likes_counter
+  def update_counter_of_likes
     post.increment!(:likes_counter)
   end
 end
