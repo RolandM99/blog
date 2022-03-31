cb2e8b27db1117f8f00bfede0c1b856caafd2732
rolandm99 <manfulmweze99@gmail.com>
2022-03-31 08:16:41 +0100

    Create likes and comments controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,29 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @user = @post.author
+    # @user = @post.author
     @comments = @post.comments
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @new_post = @user.posts.new(post_params)
+    respond_to do |myform|
+      myform.html do
+        if @new_post.save
+          redirect_to "/users/#{@new_post.author.id}/posts/"
+        else
+          render :new, alert: 'Post failed to create'
+        end
+      end
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
