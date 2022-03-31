67d7fa5636ad5da092a2a420a08d416e3d65c38f
rolandm99 <manfulmweze99@gmail.com>
2022-03-31 08:27:12 +0100

    Fix linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,10 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @add_new_comment = @post.comments.create(
-        text: comment_params[:text],
-        author_id: current_user.id,
-        post_id: @post.id
-      )
+      text: comment_params[:text],
+      author_id: current_user.id,
+      post_id: @post.id
+    )
     @add_new_comment.author_id = @post.id
     @add_new_comment.post_id = params[:post_id]
     @add_new_comment.update_comments_counter
