30da1528a331167d9ae0ff3680fe6111456cc6b8
rolandm99 <manfulmweze99@gmail.com>
2022-03-31 19:16:55 +0100

    Update routes file and environment folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'users/:user_id/posts' => 'posts#index', as: 'user_posts'
   # get 'users/:user_id/posts/:id' => 'posts#show', as: 'user_post'
   # get 'users' => 'users#index', as: 'users'
   # get 'users/:id' => 'users#show', as: 'user'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   root 'users#index'
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create]
   end
