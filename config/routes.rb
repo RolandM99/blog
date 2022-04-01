b507a6fe3e87f09a1e162ead9afb033273245700
rolandm99 <manfulmweze99@gmail.com>
2022-04-01 18:51:58 +0100

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'users/:user_id/posts' => 'posts#index', as: 'user_posts'
-  # get 'users/:user_id/posts/:id' => 'posts#show', as: 'user_post'
-  # get 'users' => 'users#index', as: 'users'
-  # get 'users/:id' => 'users#show', as: 'user'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'users#index'
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
+    namespace :api, default: {format: :json} do
+      namespace :v1,  default: {format: :json} do
+        post 'login', to: 'authentication#authenticate'
+        post 'register', to: 'users#create'
+        resources :users do
+          resources :posts, only: [:index, :show, :create] do
+            resources :comments, only: [:index, :create]
+            resources :likes, only: [:create]
+          end
+        end
+      end
+    end
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create destroy]
   end
